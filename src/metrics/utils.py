9e98ebbd2795ccf13c3ab6ff8334c6652cf01f50
Your Name <you@example.com>
2025-10-12 04:10:42 +0300

    Understood how pre-commit works

--- src/metrics/utils.py
+++ src/metrics/utils.py
@@ -3,11 +3,15 @@
 # Don't forget to support cases when target_text == ''
 
 import editdistance
+
+
 def calc_cer(target_text, predicted_text) -> float:
     assert len(target_text)
-    return editdistance.eval(target_text, predicted_text)/len(target_text)
+    return editdistance.eval(target_text, predicted_text) / len(target_text)
 
 
 def calc_wer(target_text, predicted_text) -> float:
     assert len(target_text)
-    return editdistance.eval(target_text.split(), predicted_text.split())/len(target_text.split())
+    return editdistance.eval(target_text.split(), predicted_text.split()) / len(
+        target_text.split()
+    )
--- src/model/baseline.py
+++ src/model/baseline.py
@@ -1,4 +1,6 @@
 from torch import nn
+
+
 class BaseLineModel(nn.Module):
     def __init__(self, model_dim, n_tokens, fc_hidden=512):
         super().__init__()
@@ -10,7 +12,8 @@ class BaseLineModel(nn.Module):
             nn.ReLU(),
             nn.Linear(fc_hidden, n_tokens),
         )
+
     def forward(self, spectrogram, spectrogram_lengths, **batch):
-        output_net=self.net(spectrogram.transpose(1, 2))
-        log_probs=nn.functional.log_softmax(output_net, dim=-1)
-        return {"log_probs":log_probs, "log_probs_length":spectrogram_lengths}
+        output_net = self.net(spectrogram.transpose(1, 2))
+        log_probs = nn.functional.log_softmax(output_net, dim=-1)
+        return {"log_probs": log_probs, "log_probs_length": spectrogram_lengths}
