2e05f378e07c36ed159382ce1ebcdb6d12e8ec85
Torfinhell <you@example.com>
2025-10-17 04:02:58 +0300

    Added Llm model for scoring beams. And increased beam size to 100. Ready to train in kaggle again

--- src/text_encoder/llm_scorer.py
+++ src/text_encoder/llm_scorer.py
@@ -1,16 +1,18 @@
 from typing import Any
 from transformers import GPT2LMHeadModel, GPT2Tokenizer 
 import numpy as np
+import torch
 class LLMToScore:
     """LLM-based rescoring interface for beam_search prediction"""
 
     def __init__(self, device: str = 'cpu'):
         self.device=device
-        self.model=GPT2LMHeadModel.from_pretrained('gpt2').to(self.device)
+        self.model=GPT2LMHeadModel.from_pretrained('gpt2')
         self.tokenizer=GPT2Tokenizer.from_pretrained('gpt2')
 
 
     def score(self, text):
-        token_tensor=self.tokenizer.encode(text, add_special_tokens=False, return_tensors="pt").to(self.device)
-        loss=self.model(token_tensor, labels=token_tensor)[0]
-        return np.exp(loss.cpu().detach().numpy())
+        token_tensor=self.tokenizer.encode(text, add_special_tokens=False, return_tensors="pt")
+        with torch.no_grad():
+            loss=self.model(token_tensor, labels=token_tensor)[0]
+        return np.exp(loss.cpu().detach().numpy()).item()
