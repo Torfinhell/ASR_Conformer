6438106c35c84678af43669a7dc07efb3a06a4e4
Torfinhell <you@example.com>
2025-10-19 21:48:34 +0300

    Final training

--- scripts/download_gdrive.py
+++ scripts/download_gdrive.py
@@ -1,12 +1,15 @@
 from pathlib import Path
 import gdown
 GDRIVE_URLS= {
-    "https://drive.google.com/file/d/1a1gjSXB3mMsNOHcdndhJW6ABZ8HzBEFm/view?usp=drive_link": "saved/train_clean_360_1/model_best.pth",
+    "https://drive.google.com/file/d/1a1gjSXB3mMsNOHcdndhJW6ABZ8HzBEFm/view?usp=drive_link": "data/models/model_best.pth",
     # "": "saved/train_other_500_2/model_best.pth",
 }
 
-def download_models_gzip():
+def main():
     path_gzip=Path("data/models").absolute()
     path_gzip.mkdir(exist_ok=True, parents=True)
     for url, path in GDRIVE_URLS.items():
         gdown.download(url, path)
+        
+if __name__== "__main__":
+    main()
