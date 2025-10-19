45c00398e3534a679f1b5d69d69dd0d79840505a
Torfinhell <nikita2school@icloud.com>
2025-10-19 17:03:53 +0300

    Going to start finetune

--- src/text_encoder/base_text_encoder.py
+++ src/text_encoder/base_text_encoder.py
@@ -9,13 +9,13 @@ from torch import Tensor
 
 from .llm_scorer import LLMToScore
 from hydra.utils import instantiate
+from math import exp
 class BaseTextEncoder:
     """Base class for text encoders.
 
     Args:
         alphabet: list of symbols
-        beam_size: parametr for beam-search
-        use_LLM: LLM rescoring
+        beam_size, beam_depth, take_first_tokens: parametrs for beam-search
     """
 
     EMPTY_TOK = ""
@@ -24,10 +24,14 @@ class BaseTextEncoder:
         self,
         alphabet: Optional[Iterable[str]] = None,
         beam_size: Optional[int] = None,
+        beam_depth: Optional[int] = None,
+        take_first_tokens: Optional[int] = None,
         llm_model: Any = None,
         **kwargs,
     ) -> None:
         self.beam_size = beam_size
+        self.beam_depth = beam_depth
+        self.take_first_tokens = take_first_tokens
         try:
             self.beam_values_are_intialised()
             self.is_beam_search = True
@@ -42,7 +46,6 @@ class BaseTextEncoder:
         self.ind2token = dict(enumerate(self.vocab))
         self.token2ind = {v: k for k, v in self.ind2token.items()}
         self.llm = llm_model
-        self.is_last=False
     def __len__(self) -> int:
         return len(self.vocab)
 
@@ -92,36 +95,35 @@ class BaseTextEncoder:
     def ctc_beam_search(self, log_probs: np.ndarray, length: int):
         self.beam_values_are_intialised()
         dp = {
-            ("", self.EMPTY_TOK): 1.0,
+            ("", self.EMPTY_TOK): 0.0,
         }
-        for idx in range(len(log_probs)):
-            dp = self.expand_beam_and_merge_beams(dp, log_probs[idx], length)
-            self.is_last=(idx==len(log_probs)-1)
+        for idx in range(0, len(log_probs), self.beam_depth):
+            for step in range(min(self.beam_depth, len(log_probs) - idx)):
+                dp = self.expand_beam_and_merge_beams(dp, log_probs[idx + step], length)
             dp = self.truncate_beams(dp)
-            self.is_last=False
         return dp
 
     def expand_beam_and_merge_beams(
         self, dp: dict, cur_step_log_prob: np.ndarray, maxlen: int
     ) -> dict:
         self.beam_values_are_intialised()
-        new_dp = defaultdict(float)
-        best_chars_sorted = np.argsort(cur_step_log_prob)[::-1]
-        for (pref, prev_char), prev_log_proba in dp.items():
-            for idx in best_chars_sorted:
-                char = self.ind2token[idx]
+        new_dp = defaultdict(lambda: float('-inf'))
+        best_tokens_sorted = np.argsort(cur_step_log_prob)[::-1]
+        for (pref, prev_token), prev_log_proba in dp.items():
+            for idx in best_tokens_sorted[:self.take_first_tokens]:
+                token = self.ind2token[idx]
                 cur_log_proba = prev_log_proba + cur_step_log_prob[idx]
                 cur_pref = pref
-                if char != self.EMPTY_TOK and prev_char != char:
-                    cur_pref += char
+                if token != self.EMPTY_TOK and prev_token != token:
+                    cur_pref += token
                 if len(cur_pref) <= maxlen:
-                    new_dp[(cur_pref, char)] += cur_log_proba
+                    new_dp[(cur_pref, token)]=np.logaddexp(new_dp[(cur_pref, token)],cur_log_proba)
         return new_dp
 
-    def truncate_beams(self, dp: dict, beam_size: Optional[int] = None) -> dict:
+    def truncate_beams(self, dp: dict, beam_size: Optional[int] = None, use_llm:bool=False) -> dict:
         self.beam_values_are_intialised()
         beam_size = beam_size if beam_size else self.beam_size
-        if self.llm is not None and self.is_last:
+        if use_llm and self.llm is not None:
             return dict(
                 sorted(dp.items(), key=lambda x: self.llm.score(x[0]))[:beam_size]
             )
@@ -130,7 +132,9 @@ class BaseTextEncoder:
     def beam_values_are_intialised(self):
         if (
             self.beam_size is None
+            or self.beam_depth is None
+            or self.take_first_tokens is None
         ):
             raise ValueError(
-                "One of the beam_size, beam_depth or take_first_chars is not initialised in the config"
+                "One of the beam_size, beam_depth or take_first_tokens is not initialised in the config"
             )
