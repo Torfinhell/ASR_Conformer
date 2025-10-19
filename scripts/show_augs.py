c86e5365cbc940c2731ace2d32ade38f2f85ec19
Torfinhell <you@example.com>
2025-10-19 19:35:49 +0300

    Changed script for finetune

--- scripts/show_augs.py
+++ scripts/show_augs.py
@@ -1,12 +1,21 @@
+import sys
+import sys
+from pathlib import Path
+sys.path.append(str(Path(__file__).resolve().parents[1]))
 from hydra.utils import instantiate
-
+import hydra
 from src.logger.utils import plot_spectrogram
 from src.text_encoder import CTCTextEncoder
 from src.transforms.spec_augs import MaskFreq, TimeMask
 from src.transforms.wav_augs import Gain, ShiftPitch
+from omegaconf import OmegaConf
+from src.utils.init_utils import set_random_seed, setup_saving_and_logging
 
-
-def show_augs(config, writer):
+@hydra.main(version_base=None, config_path="../src/configs", config_name="show_augs")
+def main(config):
+    project_config = OmegaConf.to_container(config)
+    logger = setup_saving_and_logging(config)
+    writer = instantiate(config.writer, logger, project_config)
     dataset = instantiate(config.datasets["train"], text_encoder=CTCTextEncoder())
     for i in range(min(config.trainer.show_examples, len(dataset))):
         writer.set_step(i, "")
@@ -29,3 +38,6 @@ def show_augs(config, writer):
 def image_spectrogram(spectrogram, config):
     image = plot_spectrogram(spectrogram, config)
     return image
+
+if __name__ == "__main__":
+    main()
